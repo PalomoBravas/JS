dfdaa5fbdd13dbbf2ff8e1d07c2bd96230ed9397
PalomoBravas <103994782+PalomoBravas@users.noreply.github.com>
2023-06-08 12:22:49 +0300

    second edit

--- 10-array-clean/script.js
+++ 10-array-clean/script.js
@@ -3,17 +3,17 @@
 const testArr = [1,2,3,4,5,6,7,8,9];
 
 function checkElement (element) {
-  return element <= 5;
+  return element <= 3;
 }
 
-function arrayClean (arr, f) {
-  let newArr = [];
-  for ( const i in arr) {
-    if (f(arr[i])) {
-      newArr.push(arr[i]);
+function arrayClean (arr, conditionFunction) {
+  const result = [];
+  for ( const element of arr) {
+    if (conditionFunction(element)) {
+      result.push(element);
     }
   }
-  return newArr;
+  return result;
 }
 
 console.log(arrayClean(testArr, checkElement));
--- 11-data-array/script.js
+++ 11-data-array/script.js
@@ -1,12 +1,20 @@
 "use strict";
 
-const REGEX = new RegExp('^(0[1-9]|[12][0-9]|3[01])([-|\\/])(0[1-9]|1[012])([-|\\/])((19|20)\\d\\d)', 'gm');
+function isValidDate(string) {
+  const dateArr = string.split('-').reverse()
+  const checkDate = new Date(dateArr.join('-'))
 
-function dataArr (arr) {
-  const filteredArr = arr.filter(el => REGEX.test(el));
-  return filteredArr.map(el => el.replace(/\//g, '-'));
+  return checkDate.getFullYear() === Number(dateArr[0])
+    && checkDate.getMonth() === Number(dateArr[1]-1)
+    && checkDate.getDate() === Number(dateArr[2])
 }
 
-const testArr = ['10-02-2024','test', '00-12-1994','01/02/2022', '36/11/2005'];
+function dataArr (arr) {
+  return arr.filter(el => el.length === 10)
+    .map(el => el.replace(/\//g, '-'))
+    .filter(el => isValidDate(el))
+}
+
+const testArr = ['10-02-2024','test', '00-12-1994','01/02/2022', '36/11/2005', '29/10-2024', '30/02/2024', '29/02/2024'];
 
 console.log(dataArr(testArr));
