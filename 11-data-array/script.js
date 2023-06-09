9a9133f3866aa512bb526ecffa31d26d8d6b99d5
PalomoBravas <103994782+PalomoBravas@users.noreply.github.com>
2023-06-09 17:37:47 +0300

    third edit

--- 11-data-array/script.js
+++ 11-data-array/script.js
@@ -1,5 +1,14 @@
 "use strict";
 
+// если формат MM/DD/YYYY меняем его на DD-MM-YYYY
+function changeFormatDate (string) {
+  if (string.match('\/')) {
+    return `${string.split('/')[1]}-${string.split('/')[0]}-${string.split('/')[2]}`;
+  } else {
+    return string;
+  }
+}
+// проверка строки на дату
 function isValidDate(string) {
   const dateArr = string.split('-').reverse()
   const checkDate = new Date(dateArr.join('-'))
@@ -11,10 +20,10 @@ function isValidDate(string) {
 
 function dataArr (arr) {
   return arr.filter(el => el.length === 10)
-    .map(el => el.replace(/\//g, '-'))
+    .map(el => changeFormatDate(el))
     .filter(el => isValidDate(el))
 }
 
-const testArr = ['10-02-2024','test', '00-12-1994','01/02/2022', '36/11/2005', '29/10-2024', '30/02/2024', '29/02/2024'];
+const testArr = ['10-02-2024','test', 'af-cd-fhgt', 'af/cd/fhgt', '00-12-1994','01/02/2022', '36/11/2005', '30/02/2024', '02/29/2024', '12/31/2020', '31-12-2020'];
 
 console.log(dataArr(testArr));
