f4e8a90f00ed250f11626711e3d0f3287acf3aed
PalomoBravas <103994782+PalomoBravas@users.noreply.github.com>
2023-06-14 12:34:24 +0300

    first edit

--- 17-calc/script.js
+++ 17-calc/script.js
@@ -1,50 +1,44 @@
 "use strict";
 
-function resetInputFields () {
-  inputOne.value = '0';
-  inputTwo.value = '0';
-}
-
 const inputOne = document.querySelector('#inputOne');
 const inputTwo = document.querySelector('#inputTwo');
-const actionBtn = document.querySelectorAll('.button');
+const keyboard = document.querySelector('.keyboard');
 const resultField = document.querySelector('.result');
 
-actionBtn.forEach(e => {
-  e.addEventListener('click', e => {
-    const action = e.target.id;
-    const numOne = Number(inputOne.value);
-    const numTwo = Number(inputTwo.value);
+function resetInputFields() {
+  inputOne.value = null;
+  inputTwo.value = null;
+}
 
-    if (numOne === 0 || numTwo === 0) {
-      resultField.innerText = 'Enter all numbers'
-      return;
-    }
+function calculate(event) {
+  const numOne = Number(inputOne.value);
+  const numTwo = Number(inputTwo.value);
 
-    switch (action) {
-      case 'add':
-        resultField.innerText =  numOne + numTwo;
-        resetInputFields();
-        break;
-      case 'subtract':
-        resultField.innerText =  numOne - numTwo;
-        resetInputFields();
-        break;
-      case 'multiply':
-        resultField.innerText = numOne * numTwo;
-        resetInputFields();
-        break;
-      case 'divide':
-        resultField.innerText = (numOne / numTwo).toFixed(5);
-        resetInputFields();
-        break;
-      default:
-        return undefined;
-    }
-  })
-})
+  if (numOne === 0 || numTwo === 0) {
+    resultField.innerText = 'Enter all numbers'
+    return;
+  }
 
+  switch (event.target.id) {
+    case 'add':
+      resultField.innerText = numOne + numTwo;
+      resetInputFields();
+      break;
+    case 'subtract':
+      resultField.innerText = numOne - numTwo;
+      resetInputFields();
+      break;
+    case 'multiply':
+      resultField.innerText = numOne * numTwo;
+      resetInputFields();
+      break;
+    case 'divide':
+      resultField.innerText = (numOne / numTwo).toFixed(5);
+      resetInputFields();
+      break;
+    default:
+      return undefined;
+  }
+}
 
-
-
-
+keyboard.addEventListener('click', event => calculate(event))
